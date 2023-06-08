ff8e21827dca9c0cddf23c8cac4122f87b40b736
Tereza Kolečkářová <126888261+kole-ter@users.noreply.github.com>
2023-06-08 13:18:55 +0200

    Update Tic-Tac-Toe_pygame_board.py

--- Tic-Tac-Toe_pygame_board.py
+++ Tic-Tac-Toe_pygame_board.py
@@ -1,23 +1,44 @@
 import pygame
 import sys
-from constants import *
 import numpy as np
 
+####Constants####
+HEIGHT = 600
+WIDTH = 600
+LINE_WIDTH= 15
 
+BOARD_ROWS = 3
+BOARD_COLS = 3
 
-pygame.init()
-screen = pygame.display.set_mode( (WIDTH, HEIGHT ))
-pygame.display.set_caption('TIC TAC TOE AI')
-screen.fill(BG_COLOUR)
+CIRCLE_RAD = 60
+CIRCLE_WIDTH = 15
+CROSS_WIDTH = 20
+CROSS_SPACE = 50
+
+#COLOURS
+BG_COLOUR = (28,170,156)
+LINE_COLOUR = (23,145,135)
+CIRCLE_COLOUR = ( 239, 231, 200)
+CROSS_COLOUR = ( 66, 66, 66)
 
 player = 1
 
 clicked_row = None
 clicked_col = None
+game_over = False
+################
 
+#SCREEN
+pygame.init()
+screen = pygame.display.set_mode( (WIDTH, HEIGHT ))
+pygame.display.set_caption('TIC TAC TOE AI')
+screen.fill(BG_COLOUR)
+
+#BOARD
 board = np.zeros((BOARD_ROWS, BOARD_COLS))
 # argument needs to be a tuple! ()
 print(board)
+
 
 def draw_lines():
     # 1 horizontale line
@@ -29,6 +50,7 @@ def draw_lines():
     # 2 vertical line
     pygame.draw.line(screen, LINE_COLOUR, (400, 0), (400, 600), LINE_WIDTH)
 
+#X AND O SYMBOLS
 def draw_figures():
     for row in range(BOARD_ROWS):
         for col in range(BOARD_COLS):
@@ -37,16 +59,21 @@ def draw_figures():
             elif board[row][col] == 2:
                 pygame.draw.line( screen, CROSS_COLOUR, ( col * 200 +CROSS_SPACE, row * 200 + 200 -CROSS_SPACE) , (col * 200 + 200 -CROSS_SPACE, row *200 +CROSS_SPACE), CROSS_WIDTH)
                 pygame.draw.line(screen , CROSS_COLOUR, ( col * 200 +CROSS_SPACE, row *200 + CROSS_SPACE), ( col * 200 + 200 -CROSS_SPACE, row * 200 + 200 -CROSS_SPACE), CROSS_WIDTH)
-
+                
+###FCE ON THE BOARD###
+#MARK SQUARE
 def mark_square(row, col, player):
     board[row][col] = player
 
+#TO ENSURE VALID INPUT OF THE PLAYER
 def available_square(row, col):
     return  board[row][col] == 0
 
+#TIE
 def is_board_full():
     return 0 not in board
 
+#CHECK FOR WIN
 def check_win(player):
     #horizontal win check
     for row in range(BOARD_ROWS):
@@ -68,7 +95,7 @@ def check_win(player):
         return True
     return False
 
-
+#WINNIG_VISUAL 
 def draw_vertical_winning_line(col, player):
     posX = col * 200 + 200/2
     if player == 1:
@@ -99,6 +126,7 @@ def draw_desc_diagonal_win_line(player):
         colour = CROSS_COLOUR
     pygame.draw.line(screen, colour, ( 15, 15), ( WIDTH-15, HEIGHT -15), 20)
 
+#FOR RESTART/REPLAY PRESS R
 def restart():
     global game_over
     screen.fill( BG_COLOUR )
@@ -109,11 +137,9 @@ def restart():
         for col in range(BOARD_COLS):
             board[row][col] = 0
 
-
-
 draw_lines()
-game_over = False
 
+#MAIN LOOP
 def main():
     
     global game_over
